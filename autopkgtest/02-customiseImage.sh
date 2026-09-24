f9b9d42314a8daa91c692025b66bddc82d8038ea
Alessio Faina <alessio.faina@canonical.com>
2026-09-24 12:15:21 +0200

    Make the scripts a little bit more modular
    Signed-off-by: Alessio Faina <alessio.faina@canonical.com>

--- autopkgtest/02-customiseImage.sh
+++ autopkgtest/02-customiseImage.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-sudo virt-copy-in -a autopkgtest-resolute-amd64.img linux-image-7.0.0-3-generic_7.0.0-3.3_amd64.deb /tmp
-sudo virt-copy-in -a autopkgtest-resolute-amd64.img linux-headers-7.0.0-3-generic_7.0.0-3.3_amd64.deb /tmp
-sudo virt-copy-in -a autopkgtest-resolute-amd64.img linux-modules-7.0.0-3-generic_7.0.0-3.3_amd64.deb /tmp
-sudo virt-copy-in -a autopkgtest-resolute-amd64.img linux-unstable-headers-7.0.0-3_7.0.0-3.3_all.deb /tmp
-sudo virt-customize -a autopkgtest-resolute-amd64.img --run-command "dpkg -i /tmp/linux-modules-7.0.0-3-generic_7.0.0-3.3_amd64.deb; dpkg -i /tmp/linux-headers-7.0.0-3-generic_7.0.0-3.3_amd64.deb; dpkg -i /tmp/linux-image-7.0.0-3-generic_7.0.0-3.3_amd64.deb; dpkg -i /tmp/linux-unstable-headers-7.0.0-3_7.0.0-3.3_all.deb"
+ABI="7.3.0-6"
+VERSION="${ABI}.6"
+
+packages=(
+    "linux-headers-${ABI}_${VERSION}_all.deb"
+    "linux-headers-${ABI}-generic_${VERSION}_amd64.deb"
+    "linux-image-${ABI}-generic_${VERSION}_amd64.deb"
+    "linux-main-modules-zfs-${ABI}-generic_${VERSION}+1_amd64.deb"
+    "linux-modules-${ABI}-generic_${VERSION}_amd64.deb"
+		"busybox_1%3a1.38.0-3ubuntu3_amd64.deb"
+		"dracut-core_112-6_amd64.deb"
+		"dracut-install_112-6_amd64.deb"
+		"dracut_112-6_all.deb"
+		"zstd_1.5.7+dfsg-4_amd64.deb"
+)
+
+sudo virt-copy-in -a autopkgtest-stonking-amd64.img "${packages[@]}" /tmp
+tmp_packages=("${packages[@]/#//tmp/}")
+
+sudo virt-customize -a autopkgtest-stonking-amd64.img \
+	--run-command "apt install -y ${tmp_packages[*]}"
--- autopkgtest/03-runAutopackagetest.sh
+++ autopkgtest/03-runAutopackagetest.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
+sudo add-apt-repository ppa:canonical-kernel-team/unstable -y
+apt install linux-headers-7.0.0-3-generic
+apt-get update
 
-BASE="/home/alessio.faina@canonical.com/canonical/dkms"
-DISTRO="resolute"
-DSC="virtualbox_7.2.6-dfsg-3ubuntu1.dsc"
-
-autopkgtest \
-  --apt-upgrade \
-  --shell-fail \
-	--summary results.txt \
-	--timeout-factor=3 \
-  ${BASE}/tests/${DSC} \
-  -- qemu --ram-size=16384 --cpus=8 ${BASE}/autopkgtests_images/autopkgtest-${DISTRO}-amd64.img
